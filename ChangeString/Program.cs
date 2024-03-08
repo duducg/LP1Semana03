b174d570cbaf5bf925ff990ce9e6ffb369233f4e
duducg <eduardo.sang.rocha@gmail.com>
2024-03-08 17:39:24 +0000

    Finished ChangeString

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,32 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //prompt user for a string input
+            Console.Write("Please input a \"string\" value: ");
+            string instring = Console.ReadLine();
+            
+            //prompt user for a char input
+            Console.Write("Please input a \"char\" value: ");
+            string inchar = Console.ReadLine();
+            char parse_char = Char.Parse(inchar);
+
+
+            //print initial string replacing the char with X
+            string new_string = "";
+            foreach (char c in instring)
+            {
+                if (c == parse_char)
+                {
+                    new_string += 'X';
+                }
+                else
+                {
+                    new_string += c;
+                }
+            }
+            Console.WriteLine($"Your censored \"string\" is {new_string}");
+            
+
         }
     }
 }
