56c6d0ffa6113e0afbdb341f911bfb40db3d598b
duducg <eduardo.sang.rocha@gmail.com>
2024-03-08 16:49:29 +0000

    Right Size first commit

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use args to access
+            foreach (string a in args)
+            {
+                Console.WriteLine(a);//works
+                
+            }
+        }
+
+    }
+}
